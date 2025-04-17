3ff090b468fd4eec17e762079430682f66e06c56
Cheppali Surekha <cheppalisurekha1234@gmail.com>
2025-04-17 21:24:02 +0530

    student_tracker.py
    A basic Python program to add new student records. Future updates will include editing, deleting, and searching features.

--- student_tracker.py
+++ student_tracker.py
@@ -1,4 +1,15 @@
 from openpyxl import load_workbook
-workbook=load_workbook('student_records.xlsx')
-sheet=workbook.active
-print("connected to sheet:",sheet.title)
+workbook = load_workbook('D:/student_records.xlsx')
+sheet = workbook.active
+next_row=sheet.max_row+1
+id=int(input("Enter student id: "))
+name=input("Enter student name: ")
+marks=int(input("Enter student marks : "))
+sheet.cell(row=next_row,column=1).value=id
+sheet.cell(row=next_row,column=2).value=name
+sheet.cell(row=next_row,column=3).value=marks
+workbook.save('D:/student_records.xlsx')
+print("added successfully")
+print("current student record:\n") 
+for row in sheet.iter_rows(values_only=True):
+    print(row)
